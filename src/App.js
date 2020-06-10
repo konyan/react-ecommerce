f98dde973c01c83959683b8ad1e4999155ee98d6
konyan <devkonyan@gmail.com>
2020-06-10 22:53:14 +0630

    setup router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from "react-router-dom"
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div >
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,7 +48,7 @@ class Directory extends Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(section => <MenuItem title={section.title} key={section.id} imageUrl={section.imageUrl} size={section.size} />)
+          this.state.sections.map(({ id, ...otherSectionProps }) => <MenuItem key={id} {...otherSectionProps} />)
         }
       </div>
     );
