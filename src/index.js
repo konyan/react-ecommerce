f7c867624a234fbe3307a29faee874146b9a0646
konyan <devkonyan@gmail.com>
2020-06-14 01:41:29 +0630

    move directory

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor} loading={null}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </Router>
   </Provider>,
